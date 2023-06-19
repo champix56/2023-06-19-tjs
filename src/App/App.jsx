57ee8d310e7d182b2c61aabfb8e3d06ca8a9d8ea
champix56 <desorbaix@free.fr>
2023-06-19 12:11:19 +0200

    usage d'un composant without props

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -1,8 +1,15 @@
 import React from "react";
 import "./App.css";
+import Button from "./components/ui/Button/Button";
 
 const App = (props) => {
-  return <div className="App"></div>;
+  return <div className="App">
+    <Button/>
+    <Button/>
+    <Button/>
+    <Button/>
+
+  </div>;
 };
 
 export default App;
