48ee490160f34267adee5ca854d38dfa3a25112a
champix56 <desorbaix@free.fr>
2023-06-19 17:28:29 +0200

    state et cycle de vie

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -2,21 +2,39 @@ import React from "react";
 import style from "./App.module.css";
 import Button from "./components/ui/Button/Button";
 class App extends React.PureComponent {
-  counter = 0;
   constructor(props) {
     super(props);
-    console.log("constructeur de App");
+    this.state={counter:0, text:'dfghjkl'}
+  }
+  componentDidUpdate(oldProps,oldState){
+    console.log(this.state.counter)
   }
   render() {
     return (
       <div className={style.App}>
         valeur de counter :{" "}
         <span style={{ fontWeight: "900", fontSize: "42pt" }}>
-          {this.counter}
+          {this.state.counter}
         </span>
         <hr />
-        <Button bgcolor="tomato">-1</Button>
-        <Button bgcolor="skyblue">+1</Button>
+        <Button
+          bgcolor="tomato"
+          onButtonClick={() => {
+            this.setState({counter:this.state.counter-1})
+            console.log(this.state);
+          }}
+        >
+          -1
+        </Button>
+        <Button
+          bgcolor="skyblue"
+          onButtonClick={() => {
+            this.setState({counter:this.state.counter+1})
+            console.log(this.state);
+          }}
+        >
+          +1
+        </Button>
       </div>
     );
   }
