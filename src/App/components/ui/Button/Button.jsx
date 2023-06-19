445a7dc8dbc82eabe7d31ace65fb05150780b5f2
champix56 <desorbaix@free.fr>
2023-06-19 14:43:04 +0200

    usage style dynamique

--- src/App/components/ui/Button/Button.jsx
+++ src/App/components/ui/Button/Button.jsx
@@ -1,7 +1,15 @@
-import React from 'react'
-import style from './Button.module.css'
-const Button=(props)=>{
-    console.log(props)
-    return <button type={props.type} className={style.Button+' btn'}>{props.children}</button>
-}
+import React from "react";
+import style from "./Button.module.css";
+const Button = (props) => {
+  console.log(props);
+  return (
+    <button
+      style={{ backgroundColor: props.bgcolor, color: props.color }}
+      type={props.type}
+      className={style.Button + " btn"}
+    >
+      {props.children}
+    </button>
+  );
+};
 export default Button;
