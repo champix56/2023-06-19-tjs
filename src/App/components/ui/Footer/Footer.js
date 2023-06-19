adfdc250b89201435f87ecdaf643f34fed76742c
champix56 <desorbaix@free.fr>
2023-06-20 00:38:39 +0200

    ui cmps

---
+++ src/App/components/ui/Footer/Footer.js
@@ -0,0 +1,10 @@
+import React from "react";
+import styles from "./Footer.module.css";
+
+const Footer = () => (
+  <div className={styles.Footer} data-testid="Footer">
+    Desorbaix / orsys &copy; 2023
+  </div>
+);
+
+export default Footer;
---
+++ src/App/components/ui/Header/Header.js
@@ -0,0 +1,10 @@
+import React from "react";
+import styles from "./Header.module.css";
+
+const Header = () => (
+  <div className={styles.Headerter} data-testid="Header">
+    Meme<span className={styles.dot}>.</span>
+    <span className={styles.react}>react</span>
+  </div>
+);
+export default Header;
