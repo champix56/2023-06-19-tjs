ba995316015b132d390fdb386b1b22c47a8883b8
champix56 <desorbaix@free.fr>
2023-06-20 14:35:25 +0200

    ajout d'etat pour le meme current en edition

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -1,25 +1,26 @@
-import React from 'react'
-import FlexV3Grow from './components/layout/FlexV3Grow/FlexV3Grow'
-import Navbar from './components/ui/Navbar/Navbar'
-import FlexH1Grow from './components/layout/FlexH1Grow/FlexH1Grow'
-import MemeForm from './components/MemeForm/MemeForm'
-import Footer from './components/ui/Footer/Footer'
-import Header from './components/ui/Header/Header'
-import { MemeSVGViewer, emptyMeme,  } from 'orsys-tjs-meme'
+import React, { useState } from "react";
+import FlexV3Grow from "./components/layout/FlexV3Grow/FlexV3Grow";
+import Navbar from "./components/ui/Navbar/Navbar";
+import FlexH1Grow from "./components/layout/FlexH1Grow/FlexH1Grow";
+import MemeForm from "./components/MemeForm/MemeForm";
+import Footer from "./components/ui/Footer/Footer";
+import Header from "./components/ui/Header/Header";
+import { MemeSVGViewer, emptyMeme } from "orsys-tjs-meme";
 const App = () => {
+  const [current, setCurrent] = useState(emptyMeme);
   return (
-    <div className='App'>
-        <FlexV3Grow>
-          <Header/>
-          <Navbar/>
-          <FlexH1Grow>
-            <MemeSVGViewer meme={emptyMeme} image={undefined} basePath=''/>
-            <MemeForm/>
-          </FlexH1Grow>
-          <Footer/>
-        </FlexV3Grow>
+    <div className="App">
+      <FlexV3Grow>
+        <Header />
+        <Navbar />
+        <FlexH1Grow>
+          <MemeSVGViewer meme={current} image={undefined} basePath="" />
+          <MemeForm />
+        </FlexH1Grow>
+        <Footer />
+      </FlexV3Grow>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
