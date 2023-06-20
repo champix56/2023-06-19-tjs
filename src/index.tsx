eaf50ff4dfe1d3fb6eca56330836c4abe1651743
champix56 <desorbaix@free.fr>
2023-06-20 23:02:09 +0200

    ajout du context de routage

--- src/index.tsx
+++ src/index.tsx
@@ -3,16 +3,20 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App/App';
 //import reportWebVitals from './reportWebVitals';
-import {Provider} from 'react-redux'
+import { Provider } from 'react-redux'
 import store from './App/store/store';
+import { BrowserRouter as Router } from 'react-router-dom'
+
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <Provider store={store}>
-    <App />
-    </Provider>
+    <Router>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </Router>
   </React.StrictMode>
 );
 
