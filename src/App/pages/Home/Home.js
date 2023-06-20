7450e8c1d3c51c349867adcdd6c2fe8a966a478e
champix56 <desorbaix@free.fr>
2023-06-20 23:13:59 +0200

    gestion des liens simples

--- src/App/pages/Home/Home.js
+++ src/App/pages/Home/Home.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Home = () => {
   return (
@@ -6,7 +7,14 @@ const Home = () => {
       <h1>Bienvenue sur le generateur de Meme</h1>
       <p>Comme la coumbacérie ou le script de Aze, le système par rapport aux diplomaties invite à booster les revenus dans la sous-régionalité, je vous en prie.</p>
       <p>D'une manière ou d'une autre, la congolexicomatisation éventualiste sous cet angle là sert à prévaloir ce système phénoménal comparé(e)(s) la rénaque, tu sais ça.</p>
-      <ul><li>thumbnail</li><li>new</li></ul>
+      <ul>
+        <li>
+            <Link to="thumbnail">thumbnail</Link>
+        </li>
+        <li>
+            <Link to="/editor">new</Link>
+        </li>
+      </ul>
     </div>
   )
 }
