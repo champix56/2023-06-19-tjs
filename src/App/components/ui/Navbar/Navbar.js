7c887aed77347987b3dc9d5a7536a2c9c8d6180e
champix56 <desorbaix@free.fr>
2023-06-20 14:35:11 +0200

    navbar ar bootstrap

--- src/App/components/ui/Navbar/Navbar.js
+++ src/App/components/ui/Navbar/Navbar.js
@@ -1,6 +1,11 @@
 import React from "react";
 import PropTypes from "prop-types";
 import styles from "./Navbar.module.css";
+import {Navbar as NavBar, Container, Nav} from 'react-bootstrap'
+import '../../../../../node_modules/bootstrap/dist/css/bootstrap.css'
+import '../../../../../node_modules/jquery/dist/jquery.js'
+import '../../../../../node_modules/bootstrap/dist/js/bootstrap.js'
+//import NavBar from 'react-bootstrap/Navbar'
 /**
  * composant Navbar
  * @param {{}} props props du composant Navbar
@@ -9,7 +14,16 @@ import styles from "./Navbar.module.css";
 const Navbar = (props) => {
   return (
     <div className={styles.Navbar} data-testid="Navbar">
-      Navbar Component
+       <NavBar bg="primary" data-bs-theme="dark">
+        <Container>
+          <NavBar.Brand href="#home">Navbar</NavBar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#features">Features</Nav.Link>
+            <Nav.Link href="#pricing">Pricing</Nav.Link>
+          </Nav>
+        </Container>
+      </NavBar>
     </div>
   );
 };
