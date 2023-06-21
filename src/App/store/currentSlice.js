db259373ff0c9725262530075d8a12a469597ae0
champix56 <desorbaix@free.fr>
2023-06-21 14:02:10 +0200

    asyncthunk

--- src/App/store/currentSlice.js
+++ src/App/store/currentSlice.js
@@ -1,23 +1,34 @@
-import { createSlice } from '@reduxjs/toolkit'
-import { emptyMeme } from 'orsys-tjs-meme';
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import { emptyMeme } from "orsys-tjs-meme";
+import { REST_ADR } from "../config/config";
 
 const initialState = {
-    meme:emptyMeme
-}
+  meme: emptyMeme,
+};
 
 const currentSlice = createSlice({
-  name: 'current',
+  name: "current",
   initialState,
   reducers: {
-    update(state,action){
-        state.meme=action.payload
+    update(state, action) {
+      state.meme = action.payload;
     },
-    clear(state){
-        state.meme=emptyMeme
+    clear(state) {
+      state.meme = emptyMeme;
+    },
+  },
+})
+
+export const { update, clear } = currentSlice.actions;
+export const save = createAsyncThunk("current/save", async (meme) => {
+  const prm = await fetch(
+    `${REST_ADR}/memes${undefined !== meme.id ? "/" + meme.id : ""}`,
+    {
+        headers:{"Content-Type":"application/json"},
+        method: undefined !== meme.id ? 'PUT' : 'POST',
+        body:JSON.stringify(meme)
     }
-  }
+  )
+  return await prm.json()
 });
-
-export const {update,clear} = currentSlice.actions
-
-export default currentSlice.reducer
+export default currentSlice.reducer;
--- src/App/store/ressourcesSlice.js
+++ src/App/store/ressourcesSlice.js
@@ -1,17 +1,35 @@
-import { createSlice } from '@reduxjs/toolkit'
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import { REST_ADR } from "../config/config";
 
 const initialState = {
-    memes:[],
-    images:[]
-}
+  memes: [],
+  images: [],
+};
 
 const ressourcesSlice = createSlice({
-  name: 'ressources',
+  name: "ressources",
   initialState,
-  reducers: {
-  }
+  reducers: {},
+  extraReducers: (builder) => {
+    builder.addCase(
+      "ressources/fetchRessources/fulfilled",
+      (state, action) => {
+        state.memes=action.payload.memes
+        state.images=action.payload.images
+      }
+    );
+  },
 });
 
-export const {update} = ressourcesSlice.actions
-
-export default ressourcesSlice.reducer
+export const {} = ressourcesSlice.actions;
+export const fetchRessources = createAsyncThunk(
+  "ressources/fetchRessources",
+  async () => {
+    const pri = await fetch(`${REST_ADR}/images`);
+    const prm = await fetch(`${REST_ADR}/memes`);
+    const images = await pri.json();
+    const memes = await prm.json();
+    return { images: images, memes: memes };
+  }
+);
+export default ressourcesSlice.reducer;
